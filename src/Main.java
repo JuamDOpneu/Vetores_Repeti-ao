02c6c9d3b23bdcc9541d8e291332d1b4938f5216
joaom <joao01062006@edu.unipar.br>
2024-09-11 19:26:58 -0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] tabuada;
+        tabuada = new int[9];
+        for (int i = 0; i < tabuada.length; i++) {
+            tabuada[i] = i;
+        }
+
+    }
+}
