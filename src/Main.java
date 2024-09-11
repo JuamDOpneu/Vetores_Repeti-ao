a768081768fd33a7a00e9bb91fbe9f8ab8520be1
joaom <joao01062006@edu.unipar.br>
2024-09-11 19:50:24 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("digite um numero para ver a tabuada! ");
         Scanner sc = new Scanner(System.in);
         int tabuada =
         sc.nextInt();
 
         //inicializaçao, condiçao, incremento
         // ++ -> vai somar +1 i= i + 1
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 101; i++) {
             System.out.println("Tabuada: " + (tabuada * i));}
 
         }
