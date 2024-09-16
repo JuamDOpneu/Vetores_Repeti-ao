b56e7e8484c63d81016743f94aa4caf363cac513
joaom <joao01062006@edu.unipar.br>
2024-09-16 20:59:21 -0300

    commit

---
+++ src/atividade2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class atividade2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero para contagem regressiva: ");
+        int num = sc.nextInt();
+
+        while (num >= 0) {
+            System.out.println(num);
+            num--;
+        }
+
+
+    }
+}
